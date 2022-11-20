083690612b7f162aa255d8dd45eb1caea8a5ac96
Jaison E Mathew <jaisonemathew14@gmail.com>
2022-11-20 13:04:29 +0530

    Removed Admin Management Module
    Removed Admin Management Module from the project

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -25,7 +25,6 @@
             <div class="wrapper">
                 <ul>
                     <li><a href="index.php">Home</a></li>
-                    <li><a href="manage-admin.php">Admin</a></li>
                     <li><a href="manage-users.php">Users</a></li>
                     <li><a href="manage-category.php">Category</a></li>
                     <li><a href="manage-food.php">Food</a></li>
